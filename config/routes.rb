a49e40867bd18d4429c858eff3ad1b5aacb71d11
karlaechartea <k.echartea@chicagobooth.edu>
2024-12-07 04:11:35 +0000

    save1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Routes for the Post resource:
+  get("/", { :controller => "boards", :action => "index" })
 
+  # Routes for the Post resource:
+ 
   # CREATE
   post("/insert_post", { :controller => "posts", :action => "create" })
           
